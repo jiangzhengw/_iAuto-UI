74e3c7fcfddf5b40194df4e74cb39f86fb3a3c33
jiangzhengw <jiangzhw19970419@163.com>
2022-06-07 15:07:23 +0800

    jzw commit -am

--- test.py
+++ test.py
@@ -11,16 +11,28 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
 driver.get("http://mail.hichina.com/")
-time.sleep(10)  # 使用强制等待，还是找不到啊
+driver.implicitly_wait(5)
+# time.sleep(3)  # 使用强制等待，还是找不到
 
 # 显示等待selenium.common.exceptions.TimeoutException: Message:
 # WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.CSS_SELECTOR, "input#username.login_input")))
-# ele = driver.find_element(By.CSS_SELECTOR, "input#username.login_input")
-# ele.click()
-# driver.find_element(By.CSS_SELECTOR, "input#username.login_input").send_keys("jiangzhw")
+# driver.switch_to.frame(2)
+# ele = driver.find_element(By.CSS_SELECTOR, "input#username")
+# ele.send_keys("jiangzhw")
+# driver.close()
+ele = driver.find_element(By.XPATH, '//*')
+print(ele)
 
-#打印pagesource后发现,确实没有元素
-with open("page_source.txt", "w+",encoding="utf-8") as f:
-    f.write(driver.page_source)
+# 打印pagesource后发现,确实没有元素,但是web前端的pagesource并不能代表全部元素
+# with open("page_source.txt", "w", encoding="utf-8") as f:
+#     f.write(driver.page_source)
+
+# 打印window标签数量
+# print(driver.window_handles) #['CDwindow-4A4805D456FBF088AAB5A403E7711C4D']
 
 
+# 通过xpath打印全部控件
+# page_eles = driver.find_element(By.XPATH, "//*")
+
+# 截图查看 ,是存在的
+# driver.get_screenshot_as_file("alibb.png")
